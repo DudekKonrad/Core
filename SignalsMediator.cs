99f930d8e94be11d2c043e39800e7a41d927518e
Konrad Dudek <kdudek111@wp.pl>
2023-12-07 01:49:10 +0100

    Added generic signals installer

---
+++ SignalsMediator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Zenject;
+
+namespace Application.GameCore
+{
+    public static class SignalsMediator
+    {
+        public static void InstallSignalBindings<T>(this DiContainer container) where T : ISignal
+        {
+            var signals = AppDomain.CurrentDomain.GetAssemblies().SelectMany(_ => _.GetTypes()).Where(_ =>
+                typeof(T).IsAssignableFrom(_) && _.IsAbstract == false && _.IsInterface == false);
+
+            foreach (var signal in signals)
+            {
+                container.DeclareSignal(signal).OptionalSubscriberWithWarning();
+            }
+        }
+    }
+
+    public interface ISignal
+    {
+        
+    }
+}

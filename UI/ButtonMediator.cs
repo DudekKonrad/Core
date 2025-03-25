ce5361984f27cdaccdee75744291ba023cf6e582
Konrad Dudek <kdudek111@wp.pl>
2025-03-25 12:19:29 +0100

    Add keyboard based navigation

--- UI/ButtonMediator.cs
+++ UI/ButtonMediator.cs
@@ -7,29 +7,30 @@ using Zenject;
 namespace Application.Core.UI
 {
     [RequireComponent(typeof(Button))]
-    public class ButtonMediator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    public class ButtonMediator : MonoBehaviour, ISelectHandler, IDeselectHandler, ISubmitHandler
     {
         [Inject] private SignalBus _signalBus;
-        
+
         [SerializeField] protected float _duration = 0.25f;
         [SerializeField] protected float _scale = 1.1f;
-        
-        protected Button Button;
 
+        protected Button Button;
+        
         private void Start()
         {
             Button = GetComponent<Button>();
         }
-        public void OnPointerEnter(PointerEventData eventData)
+
+        public void OnSelect(BaseEventData eventData)
         {
             _signalBus.Fire(new CoreSignals.PlaySoundSignal(AudioClipModel.Sounds.OnButtonHover));
             gameObject.transform.DOScale(Vector3.one * _scale, _duration);
         }
-        public void OnPointerExit(PointerEventData eventData)
+        public void OnDeselect(BaseEventData eventData)
         {
             gameObject.transform.DOScale(Vector3.one, _duration);
         }
-        public virtual void OnPointerClick(PointerEventData eventData)
+        public void OnSubmit(BaseEventData eventData)
         {
             _signalBus.Fire(new CoreSignals.PlaySoundSignal(AudioClipModel.Sounds.OnChoose));
         }

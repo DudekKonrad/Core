d48d16030b92721ccbe6ab8322274f0a2a6ba53b
Konrad Dudek <kdudek111@wp.pl>
2026-02-03 00:52:34 +0100

    Remove Unity UI Toolkit leftovers

---
+++ UI/UIConfig.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+namespace Application.Core.UI
+{
+    [CreateAssetMenu(menuName = "Core/Create UIConfig", fileName = "SoundConfig", order = 0)]
+    public class UIConfig : ScriptableObject
+    {
+        
+    }
+}
